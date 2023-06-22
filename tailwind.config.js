74910f02121a64be27605f41aa42f867ab6e187e
Nicasiomarques <nicasiomarques18@gmail.com>
2023-06-22 19:01:26 +0100

    refactor: forms and RN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+export const themeCustom = {
+  colors: {
+    black: "#121214",
+    green: "#217356",
+    greenLigth: "#238C66",
+    gray700: "#5A5A61",
+    gray500: "#202024",
+    gray800: "#797983",
+    red: "#741C1C",
+    blue: "#7AEDED",
+    white: "#FFFFFF",
+    white500: "#E8E8E8",
+  },
+  fontFamily: {
+    robotoRegular: "oboto_400Regular",
+    roboto500Medium: "Roboto_500Medium",
+    roboto700Bold: "Roboto_700Bold",
+  },
+};
+
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}", 
+    "./App.{js,jsx,ts,tsx}",
     "./src/screens/**/*.{ts,tsx}",
     "./src/components/**/*.{ts,tsx}",
-    "./src/routers/**/*.{ts,tsx}"
+    "./src/routers/**/*.{ts,tsx}",
   ],
   theme: {
-    colors: {
-      black: '#121214',
-      green: '#217356',
-      greenLigth: '#238C66',
-      gray700: '#5A5A61',
-      gray500: '#202024',
-      gray800: '#797983',
-      red: '#741C1C',
-      blue: '#7AEDED',
-      white: '#FFFFFF',
-      white500: '#E8E8E8'
-    },
-    fontFamily: {
-      'robotoRegular': 'oboto_400Regular',
-      'roboto500Medium': 'Roboto_500Medium',
-      'roboto700Bold': 'Roboto_700Bold'
-    },
-  plugins: [],
-}
-}
+    ...themeCustom,
+    plugins: [],
+  },
+};
