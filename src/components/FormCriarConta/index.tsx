d8b3346076a66e7ce7c8dfe030e12cb460e74249
Nicasiomarques <nicasiomarques18@gmail.com>
2023-06-27 23:54:00 +0100

    feat: add integration signup

--- src/components/FormCriarConta/index.tsx
+++ src/components/FormCriarConta/index.tsx
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { SelectList } from "react-native-dropdown-select-list";
-import { View, Text, Pressable, TextInput} from "react-native";
+import { View, Text, Pressable, TextInput } from "react-native";
 import Checkbox from "expo-checkbox";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 import { ButtonCancel, ButtonDone, ButtonNext } from "../Buttons";
-import { DeFaultTextInput } from "../InputBase"
+import { DeFaultTextInput } from "../InputBase";
 
 import { Curso, categories } from '../../Datas/Uteis'
-
-
 
 export function FormCriarConta() {
   const { navigate } = useNavigation();
   const [
-    { bornDate, category, hasCar, initialDate, passwordIsVisible, showCalender },
+    {
+      bornDate,
+      category,
+      hasCar,
+      initialDate,
+      passwordIsVisible,
+      showCalender,
+      name,
+      email,
+      identificationNumber,
+      password,
+    },
     setFormCreateAccount,
   ] = useState({
     category: "",
@@ -25,24 +35,55 @@ export function FormCriarConta() {
     initialDate: new Date(),
     bornDate: "",
     hasCar: false,
+    name: "",
+    email: "",
+    identificationNumber: "",
+    password: "",
+    username: ''
   });
 
   const onChangeDate = () => {
-    setFormCreateAccount(props => ({ ...props, showCalender: true }));
+    setFormCreateAccount((prev) => ({ ...prev, showCalender: true }));
   };
 
   const onChange = (_, initialDate) => {
-    setFormCreateAccount(props => ({
-      ...props,
+    setFormCreateAccount((prev) => ({
+      ...prev,
       initialDate,
       showCalender: false,
       bornDate: initialDate.toLocaleDateString(),
     }));
   };
 
+  const handleSubmit = () => {
+    axios
+      .post("https://dasoboleia.up.railway.app/api/auth/login", {
+        nome: name,
+        sobrenome: "Doe",
+        email,
+        senha: password,
+        data_nascimento: bornDate,
+        categoria: category,
+        n_identificacao: identificationNumber,
+      })
+      .then((response) => {
+        // Handle success
+        console.log(response.data);
+        // Navigate to the next screen or perform any other actions
+        navigate('login')
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
+      });
+  };
+
   return (
     <View>
-      <DeFaultTextInput placeholder={"Digite o seu nome"} />
+      <DeFaultTextInput
+        placeholder={"Digite o seu nome"}
+        onChangeText={(text) => setFormCreateAccount((prev) => ({ ...prev, name: text }))}
+      />
 
       <View className='w-full h-12 relative rounded-md my-1 flex-row'>
         <TextInput
@@ -64,12 +105,22 @@ export function FormCriarConta() {
         <DateTimePicker mode='date' display='spinner' value={initialDate} onChange={onChange} />
       ) : null}
 
-      <DeFaultTextInput placeholder={"Digite o seu nome de usuário"} />
-      <DeFaultTextInput placeholder={"Digite o seu E-mail"} keyboardType='email-address' />
-      <DeFaultTextInput placeholder={"Digite o seu Número de identificação"} />
+      <DeFaultTextInput
+        placeholder={"Digite o seu nome de usuário"}
+        onChangeText={(text) => setFormCreateAccount((prev) => ({ ...prev, username: text }))}
+      />
+      <DeFaultTextInput
+        placeholder={"Digite o seu E-mail"}
+        keyboardType='email-address'
+        onChangeText={(text) => setFormCreateAccount((prev) => ({ ...prev, email: text }))}
+      />
+      <DeFaultTextInput
+        placeholder={"Digite o seu Número de identificação"}
+        onChangeText={(text) => setFormCreateAccount((prev) => ({ ...prev, identificationNumber: text }))}
+      />
 
       <SelectList
-        setSelected={category => setFormCreateAccount(props => ({ ...props, category }))}
+        setSelected={(selected) => setFormCreateAccount((prev) => ({ ...prev, category: selected }))}
         data={categories}
         save='value'
         placeholder='Informe a Categoria'
@@ -82,7 +133,7 @@ export function FormCriarConta() {
 
       {category === "Aluno(a)" ? (
         <SelectList
-          setSelected={val => {}}
+          setSelected={(selected) => { }}
           data={Curso}
           save='value'
           placeholder='Informe o Curso'
@@ -100,13 +151,14 @@ export function FormCriarConta() {
           placeholder={"Digite a Palavra-Passe"}
           placeholderTextColor={"#5B5B60"}
           secureTextEntry={passwordIsVisible ? false : true}
+          onChangeText={(text) => setFormCreateAccount((prev) => ({ ...prev, password: text }))}
         />
         <Pressable
           className='items-center absolute top-0 right-0 justify-center w-[15%] h-full rounded-tr-md rounded-br-md bg-white500'
           onPress={() =>
-            setFormCreateAccount(props => ({
-              ...props,
-              passwordIsVisible: !props.passwordIsVisible,
+            setFormCreateAccount((prev) => ({
+              ...prev,
+              passwordIsVisible: !prev.passwordIsVisible,
             }))
           }
         >
@@ -123,17 +175,21 @@ export function FormCriarConta() {
           <Checkbox
             className='bg-white500'
             value={hasCar}
-            onValueChange={() => setFormCreateAccount(props => ({ ...props, hasCar: !hasCar }))}
+            onValueChange={() => setFormCreateAccount((prev) => ({ ...prev, hasCar: !prev.hasCar }))}
           />
           <Text className='text-white ml-2'>Tenho uma viatura</Text>
         </View>
 
         <View className='flex relative flex-row  mt-1'>
-          <ButtonCancel wrapperStyle="w-1/3 bg-red" description={"Cancelar"} action={() => {}} />
+          <ButtonCancel wrapperStyle="w-1/3 bg-red" description={"Cancelar"} action={() => { }} />
           {hasCar ? (
-            <ButtonNext wrapperStyle="w-1/3 h-12 absolute top-0 right-0 bg-greenLigth" description={"Seguinte"} action={() => navigate("registarViatura")} />
+            <ButtonNext
+              wrapperStyle="w-1/3 h-12 absolute top-0 right-0 bg-greenLigth"
+              description={"Seguinte"}
+              action={() => navigate("registarViatura")}
+            />
           ) : (
-            <ButtonDone wrapperStyle="w-1/3 h-12 absolute top-0 right-0 bg-greenLigth" description={"Concluir"} action={() => {}} />
+            <ButtonDone wrapperStyle="w-1/3 h-12 absolute top-0 right-0 bg-greenLigth" description={"Concluir"} action={handleSubmit} />
           )}
         </View>
       </View>
